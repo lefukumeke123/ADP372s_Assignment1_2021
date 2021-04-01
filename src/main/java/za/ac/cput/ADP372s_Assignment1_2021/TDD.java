99d2c5967a6914dd3b28af60667ff856d52764a9
MARVIN <marvinlamohr@outlook.com>
2021-04-01 14:53:24 +0200

    Created Project File

---
+++ src/main/java/za/ac/cput/ADP372s_Assignment1_2021/TDD.java
@@ -0,0 +1,36 @@
+package za.ac.cput.ADP372s_Assignment1_2021;
+
+/**
+ * Student Name:
+ * Student Number:
+ */
+
+public class TDD {
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "TDD{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    private String id,name;
+
+}
---
+++ src/test/java/za/ac/cput/ADP372s_Assignment1_2021/TDDTest.java
@@ -0,0 +1,13 @@
+package za.ac.cput.ADP372s_Assignment1_2021;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Student Name:
+ * Student Number:
+ */
+
+class TDDTest {
+
+}
